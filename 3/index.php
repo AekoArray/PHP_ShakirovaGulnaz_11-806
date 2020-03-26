b5ed0449028c9c35ce833237d9d549ca502a3e6d
GulnazShakirova <GulnazRafShakirova@stud.kpfu.ru>
2020-03-26 19:54:48 +0300

    fix 3

--- 3/index.php
+++ 3/index.php
@@ -1,43 +1,27 @@
 <?php
-echo "Извините за ужасный код :с"."</br>";
 $array = array();
+$arrayStr = array();
 if (isset($_POST['button'])){
     $str = explode("\n", $_POST["string"]);
     foreach ($str as $string){
         array_push($array, $string);
         $arr = explode(" ", $string);
         shuffle($arr);
-        $arr = implode(" ", $arr);
-        array_push($array, $arr);
+        array_push($arrayStr, $arr);
     }
-    mix($array);
+    mix($arrayStr);
 } else{
     include "form.html";
     return;
 }
-function mix($array)
-{
-    for ($i = 0; $i <= count($array) - 1; $i++) {
-                if (($i + 1) == count($array)){
-                    $el = explode(" ", $array[$j]);
-                    $el2 = explode(" ",  $array[$i]);
-        } else {
-            $j = $i + 1;
-                    $el = explode(" ", $array[$i]);
-                    $el2 = explode(" ",  $array[$j]);
-        }
-                $newarr = array();
-                array_push($newarr, $el[1]);
-                array_push($newarr, $el2[1]);
-                if (array_key_first($newarr) != 0){
-                                $buf = $array[$j];
-                                $array[$j] = $array[$i];
-                                $array[$i] = $buf;
-                }
+function mix($array){
+    usort($array, function ($a, $b){
+        return strcasecmp($a[1], $b[1]);
+    });
+
+    foreach ($array as $item){
+        print implode(" ", $item) . "</br>";
     }
-            foreach ($array as $item) {
-            echo $item . "</br>";
-        }
 }
 
 
