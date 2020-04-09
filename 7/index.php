b598202d51ea49c157433188d18aca58af2cff93
GulnazShakirova <GulnazRafShakirova@stud.kpfu.ru>
2020-04-09 23:59:14 +0300

    fix 7

--- 7/index.php
+++ 7/index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if (isset($_POST['button'])){
     $str = $_POST["string"];
     echo $str;
@@ -12,11 +11,22 @@ if (isset($_POST["ping"])){
     exec("ping ".$str, $arr);
     var_dump($arr);
     $ping = implode(" ", $arr);
-        for ($i = strripos($ping, "[") + 1; $i < strripos($ping, "}" - 1); $i++) {
-
+        for ($i = strripos($ping, "[") + 1; $i < strripos($ping, "]") - 1; $i++) {
+echo "<b>".$ping[$i]."</b>";
     }
+        echo "</br>";
+    for ($i = strripos($ping, "(") + 1; $i < strripos($ping, "%") - 1; $i++) {
+        $str = $str.$ping[$i];
+    }
+    echo 100-(int)$str;
+    echo "%";
 } else if(isset($_POST["tracert"])){
-
+    exec("tracert ".$str, $arr);
+        foreach ($arr as $value){
+            if (preg_match("&[0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}&", $value)){
+                echo $value." ";
+            }
+        }
 } else {
     echo "Выберите ping или tracert";
 }
